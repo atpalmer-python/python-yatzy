8c611c0c467e9f95e2b331580b07c38ef8855902
Andrew Palmer <atp@sdf.org>
2020-08-16 18:59:14 -0500

    Add Roll tests

--- test/test_yahtzee.py
+++ test/test_yahtzee.py
@@ -1,3 +1,4 @@
+import pytest
 import yahtzee
 
 
@@ -9,4 +10,15 @@ def test_Roll_item_access():
     roll = yahtzee.Roll()
     for i in range(0, 5):
         assert roll[i] >= 1 and roll[i] <= 6
+
+
+def test_Roll_item_access_out_of_bounds():
+    roll = yahtzee.Roll()
+    with pytest.raises(IndexError):
+        assert roll[5] == None
+
+
+def test_Roll_iter():
+    for die in iter(yahtzee.Roll()):
+        assert die >= 1 and die <= 6
 
