133c505c7dd4a1ddd3a16b85117b595482bcfea7
Andrew Palmer <atp@sdf.org>
2020-08-16 15:18:17 -0500

    Implement die value access

--- test/test_yahtzee.py
+++ test/test_yahtzee.py
@@ -4,3 +4,9 @@ import yahtzee
 def test_Roll():
     roll = yahtzee.Roll()
 
+
+def test_Roll_item_access():
+    roll = yahtzee.Roll()
+    for i in range(0, 5):
+        assert roll[i] >= 1 and roll[i] <= 6
+
