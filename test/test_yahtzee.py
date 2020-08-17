8a780ce6a26c27c63b08aaa236bdd1b3c4447ef8
Andrew Palmer <atp@sdf.org>
2020-08-16 23:23:28 -0500

    Test Roll initialization

--- test/test_yahtzee.py
+++ test/test_yahtzee.py
@@ -2,7 +2,17 @@ import pytest
 import yahtzee
 
 
+
 def test_Roll_item_access():
+    roll = yahtzee.Roll(1,2,3,4,5)
+    assert roll[0] == 1
+    assert roll[1] == 2
+    assert roll[2] == 3
+    assert roll[3] == 4
+    assert roll[4] == 5
+
+
+def test_Roll_random_creation():
     roll = yahtzee.Roll.roll()
     for i in range(0, 5):
         assert roll[i] >= 1 and roll[i] <= 6
