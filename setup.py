c6cce5ed0a17eedcaa6319975e8382cb5df2652e
Andrew Palmer <atp@sdf.org>
2020-08-18 14:57:18 -0500

    Update setup.py for distribution

--- setup.py
+++ setup.py
@@ -3,12 +3,25 @@ from setuptools import setup, Extension
 
 setup(
     name='yatzy',
+    version='0.1.0',
+    author='Andrew Palmer',
+    author_email='atp@sdf.org',
+    description='Yatzy game framework',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/atpalmer-python/python-yatzy',
+
     ext_modules=[
         Extension('yatzy', sources=[
             'src/yatzy.c',
             'src/roll.c',
             'src/scorecard.c',
         ])
-    ]
+    ],
+
+    classifiers=[
+    ],
+
+    python_requires=">=3.7",
 )
 
